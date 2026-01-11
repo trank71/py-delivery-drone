09ed98aae01cfaabd04c448b1fcbc220cf93a9ab
Ambra <trank13f@gmail.com>
2026-01-11 08:40:05 +0100

    created class BaseRobot, with methods to move robot by cord

--- app/main.py
+++ app/main.py
@@ -2,4 +2,28 @@ class Cargo:
     def __init__(self, weight: int) -> None:
         self.weight = weight
 
-# write your code here
+class  BaseRobot:
+    def __init__(self
+                 ,name: str
+                 , weight: int
+                 , coords: list | None = None
+                 ) -> None:
+        self.weight = weight
+        self.name = name
+        self.coords = coords or [0, 0]
+
+    def get_info(self) -> str:
+        return f"Robot: {self.name}, Weight: {self.weight}"
+
+    def go_forward(self, step = 1) -> None:
+        self.coords[1] += step
+
+    def go_back(self, step = 1) -> None:
+        self.coords[1] -= step
+
+    def go_right(self, step = 1) -> None:
+        self.coords[0] += step
+
+    def go_left(self, step = 1) -> None:
+        self.coords[0] -= step
+
