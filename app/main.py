5b9e863199f84681f487581bcf81bcaa24c1f2ca
Ambra <trank13f@gmail.com>
2026-01-11 08:58:34 +0100

    little bit change procesing coords arg, created FlyingRobot class

--- app/main.py
+++ app/main.py
@@ -10,20 +10,37 @@ class  BaseRobot:
                  ) -> None:
         self.weight = weight
         self.name = name
-        self.coords = coords or [0, 0]
+        self.coords = coords if coords is not None else [0, 0]
 
     def get_info(self) -> str:
         return f"Robot: {self.name}, Weight: {self.weight}"
 
-    def go_forward(self, step = 1) -> None:
+    def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
 
-    def go_back(self, step = 1) -> None:
+    def go_back(self, step: int = 1) -> None:
         self.coords[1] -= step
 
-    def go_right(self, step = 1) -> None:
+    def go_right(self, step: int = 1) -> None:
         self.coords[0] += step
 
-    def go_left(self, step = 1) -> None:
+    def go_left(self, step: int = 1) -> None:
         self.coords[0] -= step
+
+
+class FlyingRobot(BaseRobot):
+    def __init__(self
+                 , name: str
+                 , weight: int
+                 , coords: list | None = None
+                 ) -> None:
+        coords = coords if coords is not None else [0, 0, 0]
+        super().__init__(name, weight, coords[:2])
+        self.coords = coords
+
+    def go_up(self, step: int = 1) -> None:
+        self.coords[2] += step
+
+    def go_down(self, step: int = 1) -> None:
+        self.coords[2] -= step
 
